f78def0dd583d7ca147674a8f001f12b747b3f65
Richard <skynet.tw@gmail.com>
2020-10-27 15:22:59 +0800

    add homepage and index.html

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from mainsite.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
+
+admin.site.register(Post, PostAdmin)
+
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
 
-# Create your views here.
+def homepage(request):
+    lottos = list()
+    for i in range(6):
+        lottos.append(random.randint(1, 42))
+
+    return render(request, "index.html", locals())
+    
